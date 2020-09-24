2c5a5be5b1b670b67c328fe0ec445458be2c7d3b
Steven Cheong <stevenc49@gmail.com>
2020-09-24 16:41:21 -0700

    (cppdf.xml generateCPfeed.py)

--- generateCPfeed.py
+++ generateCPfeed.py
@@ -42,7 +42,7 @@ def main():
     for index, row in filteredDf.iterrows():
         itemList += generateItem(row['title'] + "(pdf)", getPdfUrl(row['lessonId'], row['levelShowCode'], row['hashKey'])) +"\n"
 
-    FileUtil.saveToFile(BOILERPLATE % ('CP Dialog', itemList), OUTPUT_FILE_DIR + 'cppdf.xml') 
+    FileUtil.saveToFile(BOILERPLATE % ('CP PDF', itemList), OUTPUT_FILE_DIR + 'cppdf.xml') 
 
 
 
