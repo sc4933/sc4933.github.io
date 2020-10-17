dfe85fb4ade23fcca89a5c53313709b35dc561d9
Steven Cheong <stevenc49@gmail.com>
2020-10-17 00:13:20 -0700

    (downloadBySheet.py concatDialog.py)

--- downloadBySheet.py
+++ downloadBySheet.py
@@ -6,15 +6,18 @@ import urllib
 from bs4 import BeautifulSoup
 import stringcase
 
+# globals
 MASTER_FILE = '/home/steve/tmp/chinesepod/chinesepodLessons.xlsx'
 OUTPUT_FILE_DIR = '/home/steve/workspace/sc4933.github.io/downloads'
-
 S3_URL = "http://s3contents.chinesepod.com/" 
+
+# configs
+SHEET_NAME = '2020-10-17-206-479'
 
 def main():
 
-    df = pd.read_excel(MASTER_FILE, sheet_name='upper_intemediate')
-    df = sqldf("select * from df where lessonId>=1834")   
+    df = pd.read_excel(MASTER_FILE, sheet_name=SHEET_NAME)
+    df = sqldf("select * from df")   
     print(df)
 
     for index, row in df.iterrows():
